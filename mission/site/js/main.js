4d9778a5c1106e73f44fec3eb4215b0cfda10f05
Jonathan Lindqvist <jonathan.lindqvist@itggot.se>
2017-03-20 11:34:01 +0100

    Mer fixande
    framisda i princip klar för mobilen
    javascript fixande o hamburgare

--- mission/site/js/main.js
+++ mission/site/js/main.js
@@ -1,9 +1,20 @@
-function colorToggle() {
-    element = document.querySelector(".lower_right");
-    element.classList.toggle("colorToggle");
+// function colorToggle() {
+//     element = document.querySelector(".lower_right");
+//     element.classList.toggle("colorToggle");
+// }
+//
+// function colorToggle2() {
+//     element = document.querySelector(".lower_left");
+//     element.classList.toggle("colorToggle");
+// }
+
+function colorToggle(banan) {
+    banan.classList.toggle("colorToggle");
 }
 
-function colorToggle2() {
-    element = document.querySelector(".lower_left");
-    element.classList.toggle("colorToggle");
-}
+// function reset(this){
+//   elements = document.querySelectorAll(".red");
+//   elements.forEach(function (element) {
+//     element.classList.remove("red");
+//   });
+// }
